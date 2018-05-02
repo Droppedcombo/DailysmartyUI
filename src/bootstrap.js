d4772d61cf61e327df7d31ed4508d8e93c8123ac
benrojas <droppedcombo7@gmail.com>
2018-05-02 14:12:28 -0600

    setup Results component / results page

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/home';
 import reducers from './reducers';
+import Results from './components/results';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -17,8 +19,8 @@ function main() {
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' component={Home}/>
-          <Route path='/results' component={Home}/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Logo from './logo';
 import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
@@ -8,11 +7,9 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div>
-           <Logo />
-           <SearchBar />
-           <RecentPosts />
-         </div>
+        <Logo />
+        <SearchBar /> 
+        <RecentPosts />
       </div>
     );
   }
