bef188ef15e55229a0119cb0cc6e411b93220dbb
benrojas <droppedcombo7@gmail.com>
2018-05-08 11:48:03 -0600

    adjusted links on results tab

--- src/components/post.js
+++ src/components/post.js
@@ -45,6 +45,9 @@ class Post extends Component {
                 </div>
            </div> )
         })
+        if(links ==0) {
+            return <div className="no-content"> No post links </div>
+        } 
      return links;
     }
 
